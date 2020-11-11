ff524c32db5e18794f71f2ad69bc3a8d08ac5849
Sangeeth Reddy <35250966+sangeeth93@users.noreply.github.com>
2020-11-11 17:22:40 +0530

    Initial Commit

---
+++ crop_images.py
@@ -0,0 +1,148 @@
+import glob,cv2,json,sys,pdb,pickle, os, tqdm
+import numpy as np
+
+def load_vid_frames(path):
+    op={}
+    if os.path.isfile(path):
+        vidcap=cv2.VideoCapture(path)
+        f=-1
+        while (vidcap.isOpened()):
+            success,image=vidcap.read()
+            if success==True:
+                f+=1
+                op[f]=image
+            else:
+                break
+    else:
+        print(path," doesnt exist")
+        sys.exit()
+    return op
+
+def find_valid_center(x,y, srch_x, srch_y):
+    new_xc = np.random.randint(0+(int(x/2)+1),srch_x-(int(x/2)+1))
+    new_yc = np.random.randint(0+(int(y/2)+1),srch_y-(int(y/2)+1))
+    return new_xc, new_yc
+    
+def load_image(f_no, loc,mode, tar_x, tar_y):
+    image = dct[f_no]
+    if mode == "target":
+        op = image[loc[1]:loc[3],loc[0]:loc[2]]
+        x_scale_factor = (loc[2]-loc[0])/tar_x
+        y_scale_factor = (loc[3]-loc[1])/tar_y
+        return [cv2.resize(op,(tar_x,tar_y)),[x_scale_factor,y_scale_factor]]
+    elif mode == "search":
+        return image
+
+def random_center_search_image(img,loc,sf,pair,srch_x, srch_y):
+    # pdb.set_trace()
+    loc_1 = loc.copy()
+    op = np.zeros((srch_y,srch_x,3))
+    width = int(img.shape[1] * sf[0])
+    height = int(img.shape[0] * sf[1])
+    loc[2] = int(loc[2] * sf[0]) 
+    loc[0] = int(loc[0] * sf[0])
+    loc[1] = int(loc[1] * sf[1])
+    loc[3] = int(loc[3] * sf[1])
+    img = cv2.resize(img,(width, height),fx=sf[0],fy=sf[1]) #scaling image to the input scaling factor
+    #find the center of scaled image
+    x_size = loc[2]-loc[0]
+    y_size = loc[3]-loc[1]
+    x_c = loc[0] + int(x_size/2)
+    y_c = loc[1] + int(y_size/2) 
+    
+    new_xc , new_yc = find_valid_center(x_size,y_size,srch_x,srch_y)
+#     print(new_xc,new_yc,x_size,y_size)
+    #padding the image if necessary
+    left_pad = 0
+    right_pad = 0
+    top_pad = 0
+    bot_pad = 0
+    if y_c-new_yc < 0: #If center cannot accomodate left context.
+        left_pad = new_yc-y_c #pad the number of values it is short of
+    if y_c+(srch_y-new_yc) > img.shape[0]: #If center cannot accomodate right context.
+        right_pad = srch_y-new_yc+y_c-img.shape[0] #pad the number of values it is short of
+    if x_c-new_xc < 0:
+        top_pad = new_xc-x_c
+    if x_c+(srch_x-new_xc) > img.shape[1]:
+        bot_pad = srch_x-new_xc+x_c-img.shape[1]
+    mean_val = np.mean(img,axis=(0,1))
+    try:
+        if left_pad or right_pad or top_pad or bot_pad:
+    #         print(loc_1)
+            if left_pad:
+                y1 = 0
+            else:
+                y1 = y_c-new_yc
+            if right_pad:
+                y2 = img.shape[0]
+            else:
+                y2 = y_c+(srch_y-new_yc)
+            if top_pad:
+                x1 = 0
+            else:
+                x1 = x_c-new_xc
+            if bot_pad:
+                x2 = img.shape[1]
+            else:
+                x2 = x_c+(srch_x-new_xc)
+            op_tmp = img[y1:y2,x1:x2]
+            op[left_pad:srch_y-right_pad,top_pad:srch_x-bot_pad] = op_tmp
+            op[:,:,0][op[:,:,0]==0] = mean_val[0]
+            op[:,:,1][op[:,:,1]==0] = mean_val[1]
+            op[:,:,2][op[:,:,2]==0] = mean_val[2]
+            return op,new_xc,new_yc,int(x_size/2),int(y_size/2),1
+        else:
+            return img[y_c-new_yc:y_c+(srch_y-new_yc),x_c-new_xc:x_c+(srch_x-new_xc)],new_xc,new_yc,int(x_size/2),int(y_size/2),0
+    except:
+        pdb.set_trace()
+
+
+Video_Location = "/Volumes/1TB/Text_For_Autonomous_Navigation/Videos/train/100/"
+with open("prestep/100.txt",'rb') as f:
+    chk=pickle.load(f)
+
+tar_x = 86
+tar_y = 46
+srch_x = 172
+srch_y = 92
+
+folders = ["0","100","200","300","400"]
+for fldr in folders:
+    Video_Location = "/Volumes/1TB/Text_For_Autonomous_Navigation/Videos/train/"+fldr+"/"
+    with open("prestep/"+fldr+".txt",'rb') as f:
+        chk=pickle.load(f)
+    final_dump = []
+    i=0
+    prev_vid_name = Video_Location + str(chk[0][0])+".mp4"
+    dct = load_vid_frames(prev_vid_name)
+    for pair in tqdm.tqdm(chk):
+        vid_name = Video_Location +str(pair[0])+".mp4"
+        if vid_name != prev_vid_name:
+            dct = load_vid_frames(vid_name)
+            prev_vid_name = vid_name
+        target_frame_no = pair[1]
+        search_frame_no = pair[2]
+        target_loc = pair[3]
+        search_loc = pair[4]
+        try:
+            tar_image, sf = load_image(target_frame_no, target_loc,"target", tar_x, tar_y)
+            srch_image = load_image(search_frame_no, search_loc,"search", tar_x, tar_y)
+            if tar_image.any() and srch_image.any():
+                tar_image = cv2.resize(tar_image,(86,46))
+                srch_image,xc,yc,x,y = random_center_search_image(srch_image,search_loc,sf,pair)
+                final_dump.append([tar_image,srch_image,[xc,yc,x,y]])
+                i+=1
+                if i>=1000:
+                    with open("/Volumes/1TB/pb_files/friyay.pb","ab") as x:
+                        pickle.dump(final_dump,x)
+                        final_dump = []
+                        i=0
+        #         cv2.rectangle(srch_image,(xc-x,yc-y), (xc+x,yc+y), (255,0,0), 3)
+        #         cv2.imwrite("random.jpg",srch_image)
+        #         sys.exit()
+            else:
+                print("no op",pair)
+        except:
+            pass
+    with open("/Volumes/1TB/pb_files/friyay.pb","ab") as x:
+        pickle.dump(final_dump,x)
---
+++ data_prestep.py
@@ -0,0 +1,77 @@
+import glob,cv2,json,sys,pdb,pickle
+import numpy as np
+
+
+#RoadText-1k Categories:
+# (1)   English_Legible
+# (2)   Non_English_Legible
+# (3)   Illegible
+
+#RoadText Categories:
+
+# (1)   English
+# (2)   European
+# (3)   Illegible
+# (4)   Asian
+
+def load_data(path):
+    #Load data from the json file with the text locations and track annotations.
+    #json file to be in the format of Scalabel tool output.
+    with open(path,"r") as f:
+        gt_raw = json.load(f)
+    gt={}
+    for item in gt_raw:
+      if item["videoName"] not in gt.keys():
+        gt[item["videoName"]]={}
+      if item["index"] not in gt[item["videoName"]].keys():
+        gt[item["videoName"]][item["index"]]=[]
+      if item["labels"] != None:
+        gt[item["videoName"]][item["index"]].append(item["labels"])
+    return gt
+
+def check_labels_in_frame(vid,f,s,gt):
+    #check if the same text instance is present in the following frames of distance upto s.
+    #return a list of items with all the corresponding text locations in the frames.
+    tmp_lst=[]
+    if f in gt[vid].keys() and s in gt[vid].keys():
+        lbls2 = gt[vid][s]
+        lbls1 = gt[vid][f]
+        for lbl1 in lbls1:
+          for lbl2 in lbls2:
+            # print(lbl1,lbl1[0])
+            # print(lbl2,lbl2[0])
+            # sys.exit()
+            # print(lbl1["id"],lbl2[0]["id"])
+            if (lbl1[0]["id"] == lbl2[0]["id"]) and ((lbl1[0]["category"] == "European" and lbl2[0]["category"] == "European") or (lbl1[0]["category"] == "English" and lbl2[0]["category"] == "English") or (lbl1[0]["category"] == "Asian" and lbl2[0]["category"] == "Asian")):
+            # if (lbl1[0]["id"] == lbl2[0]["id"]) and ((lbl1[0]["category"] == "English_Legible" and lbl2[0]["category"] == "English_Legible") or (lbl1[0]["category"] == "Non_English_Legible" and lbl2[0]["category"] == "Non_English_Legible")):
+                b1 = [round(lbl1[0]["box2d"]["x1"]),round(lbl1[0]["box2d"]["y1"]),round(lbl1[0]["box2d"]["x2"]),round(lbl1[0]["box2d"]["y2"])]
+                b2 = [round(lbl2[0]["box2d"]["x1"]),round(lbl2[0]["box2d"]["y1"]),round(lbl2[0]["box2d"]["x2"]),round(lbl2[0]["box2d"]["y2"])]
+                tmp_lst.append([vid,f,s,b1,b2,1])
+    return tmp_lst
+
+folder = "Eur_5"
+gt = load_data("/Volumes/1TB/RoadText4k/"+folder+"/"+folder+"_results.json")            
+vids =glob.glob("/Volumes/1TB/RoadText4k/"+folder+"/Videos/*.mp4")
+lst=[]
+# print(vids)
+
+
+temporal_distance = 5
+for v_name in vids:
+    print(v_name)
+    vid = v_name.split("/")[-1].split(".")[0]
+    vidcap=cv2.VideoCapture(v_name)
+    frame_no = 0
+    while (vidcap.isOpened()):
+        success,image=vidcap.read()
+        if success==True:
+            for i in range(1,6):
+                t_lst = check_labels_in_frame(vid,frame_no,frame_no+i,gt)
+                if t_lst:
+                    lst=lst+t_lst
+            frame_no+=1
+        else:
+            break
+
+with open("prestep/"+folder+".txt",'wb') as f:
+    pickle.dump(lst,f)
